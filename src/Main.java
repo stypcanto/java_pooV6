77ed650a6b8ad4941b6eabb30a4e7719aa40e628
Styp Canto <styp611@outlook.com>
2025-07-27 12:47:26 -0500

    Primera version

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creamos un objeto de tipo Persona y lo inicializamos con nombre y edad
+        Persona persona1 = new Persona("Carlos", 28);
+
+        // Usamos el método saludar para mostrar un mensaje
+        persona1.saludar(); // Imprime: Hola, soy Carlos tengo 28 años
+
+        // Cambiamos la edad usando el setter
+        persona1.setEdad(29);
+
+        // Volvemos a saludar con la nueva edad
+        persona1.saludar(); // Imprime: Hola, soy Carlos tengo 29 años
+
+        // También podemos acceder a los datos usando getters
+        System.out.println("Nombre: " + persona1.getNombre());
+        System.out.println("Edad: " + persona1.getEdad());
+
+        // 1. Creamos un objeto (una instancia de la clase Persona)
+        Persona persona2 = new Persona("Ana", 25);
+
+        // 2. Llamamos a un método para que salude
+        persona2.saludar(); // Imprime: Hola, soy Ana tengo 25 años
+
+        // 3. Cambiamos su edad usando un método (encapsulamiento)
+        persona2.setEdad(26);
+
+        // 4. Volvemos a usar otro método
+        System.out.println("Nueva edad: " + persona2.getEdad()); // Imprime: Nueva edad: 26
+
+        Persona persona3 = new Persona("Juan", 25);
+    persona3.inscripcion();
+    }
+}
---
+++ src/Persona.java
@@ -0,0 +1,48 @@
+// Definimos una clase llamada Persona
+public class Persona {
+
+    // Atributos privados: representan el estado de cada objeto Persona
+    // 'private' significa que solo se puede acceder a estos datos desde esta clase
+    private String nombre;
+    private int edad;
+
+    // Constructor: se llama automáticamente cuando se crea un nuevo objeto
+    // Sirve para inicializar los valores de nombre y edad al crear una Persona
+    public Persona(String nombre, int edad){
+        // 'this.nombre' se refiere al atributo de la clase
+        // 'nombre' (sin this) es el parámetro recibido
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Método "getter" para obtener el valor del atributo 'nombre'
+    public String getNombre(){
+        return nombre;
+    }
+
+    // Método "setter" para cambiar el valor del atributo 'nombre'
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    // Getter para obtener el valor de 'edad'
+    public int getEdad(){
+        return edad;
+    }
+
+    // Setter para cambiar el valor de 'edad'
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    // Método que imprime un saludo.
+    // Este método usa los atributos del objeto para formar un mensaje.
+    public void saludar(){
+        System.out.println("Hola, soy " + nombre + " tengo " + edad + " años");
+    }
+
+    public void inscripcion(){
+        System.out.println("Buen dia, mi nombre es "+nombre+" deseo inscribirme a la universidad, tengo "+edad+ "años");
+
+    }
+}
